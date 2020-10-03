0afec1de5059627747402b42b4003b5ca57ccdde
mengjianlee <mengjianlee@gmail.com>
2020-10-03 21:51:19 +0800

    initial commit

---
+++ testIOS/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testIOS
+//
+//  Created by Lee Meng Jian  on 03/10/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world1!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testIOS/testIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  testIOSApp.swift
+//  testIOS
+//
+//  Created by Lee Meng Jian  on 03/10/2020.
+//
+
+import SwiftUI
+
+@main
+struct testIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
